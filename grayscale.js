aa4d4c04b66d39475c4df54ade2dfe84a1adc1ff
mdbaum <mdbaum@umich.edu>
2016-02-20 21:34:53 -0500

    Update grayscale.js

--- grayscale.js
+++ grayscale.js
@@ -6,10 +6,18 @@
 
 // jQuery to collapse the navbar on scroll
 
+snowy = false;
+windy = false;
+cold = false;
+hot = false;
+average = false;
+rainy = false;
+
 
 $(document).ready(function(){
                   
                   intro();
+                  whatToWear();
                   });
 
 function intro()
@@ -22,12 +30,46 @@ function intro()
               
               returnValue = person;
               mainTemp(returnValue);
+              hotCold(returnValue);
+
+              
               }
               });
 }
 /*!
  * Weather Rating Functions
  */
+
+function whatToWear() {
+    console.log(snowy);
+    console.log(windy);
+    console.log(rainy);
+    console.log(average);
+    console.log(cold);
+    console.log(hot);
+    if (snowy == "true") {
+        $('#description').text("It's snowing! We recommend bundling up in a coat, scarf, snow boots and mittens!");
+    }
+    else if (windy == "true") {
+        $('#description').text("It's windy today. Throw on some light layers--a tshirt, jacket, scarf and boots.");
+    }
+    else if (rainy == "true") {
+        $('#description').text("It's raining cats and dogs! We recommend wearing boots, pants, a rain jacket, and of course an umbrella!");
+    }
+    else if (average == "true") {
+        console.log("hello");
+        $('#description').text("Not too hot, not too cold. Put on some jeans, a tshirt, sneakers and a cap.");
+    }
+    else if (cold == "true") {
+        $('#description').text("Brrr, it's cold in here...Layer up with a sweater, jacket, hat, and gloves");
+    }
+    else {
+        console.log(average);
+        console.log(hot);
+        $('#description').text("It's swimsuit weather!! Put on your bathing suit, sun hat, shorts, and shades");
+    }
+    
+}
 function toCelsius(fahrenheit) {
     return (5/9) * (fahrenheit-32);
 }
@@ -45,58 +87,41 @@ function mainTemp(obj) {
     city = obj.name;
     $('#city').text(city);
 }
-
-function snow(obj) {
+function hotCold(obj) {
     temp = obj.main.temp;
     temp = toFahrenheit(temp);
-    if (temp < 32)
-    {
-        return "Snowing";
-    }
-    else if (31 < temp < 34) {
-        return "Potential Snow";
-    }
-    else {
-        return "No snow";
-    }
-}
-
-function windy(obj) {
-    wind = obj.wind.speed;
-    if (wind < 5) {
-        
-        return "Light Wind";
-    }
-    else if (wind < 28) {
-        return "Gentle Moderate Wind";
-    }
-    else if (wind < 61) {
-        return "Strong Wind";
-    }
-    else if (wind < 102) {
-        return "Gale";
-    }
-    else {
-        return "Storm";
-    }
-}
-
-function rain(obj) {
+    w = obj.wind.speed;
     main = (obj.weather[0]).main;
-    //rain = obj.main.rain."3h";
-    if (main == "Rain" && rain > 0.3) {
-        return "Heavy Rain";
+    console.log(temp);
+    if (temp < 32) {
+        snowy = "true";
     }
-    else if (main == "Rain") {
-        return "Rain";
+    else if (main == "Rain" && rain > 0.3) {
+        rainy = "true";
     }
-    else if (0 <rain < 0.15) {
-        return "Light Rain";
+    else if (w > 61) {
+        windy = "true";
     }
-    else {
-        return "No Rain";
+    else if (temp < 41)
+    {
+        cold = "true";
     }
+    else if (temp < 71)
+    {
+        average = "true";
+        console.log(average);
+        console.log("fuck");
+    }
+    else
+    {
+        hot = "true";
+        console.log(hot);
+        console.log("hot");
+    }
+    
+
 }
+
 
 function unixTimeConvertor(obj) {
     dt = alert(obj.dt);
@@ -125,170 +150,165 @@ jQuery(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 jQuery(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+       $('a.page-scroll').bind('click', function(event) {
+                               var $anchor = $(this);
+                               $('html, body').stop().animate({
+                                                              scrollTop: $($anchor.attr('href')).offset().top
+                                                              }, 1500, 'easeInOutExpo');
+                               event.preventDefault();
+                               });
+       });
 
 // Closes the Responsive Menu on Menu Item Click
 jQuery('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-    $('.navbar-toggle:visible').click();
-  }
-});
+                                         if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+                                         $('.navbar-toggle:visible').click();
+                                         }
+                                         });
 
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
-});
 
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
-
+    zoom: 15,
+        
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
+        
         // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
-
-        // How you would like to style the map. 
+    disableDefaultUI: true,
+    scrollwheel: false,
+    draggable: false,
+        
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 29
-            }, {
-                "weight": 0.2
-            }]
-        }, {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "saturation": 36
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 40
-            }]
-        }, {
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 19
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }, {
-                "weight": 1.2
-            }]
-        }]
+    styles: [{
+             "featureType": "water",
+             "elementType": "geometry",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 17
+                         }]
+             }, {
+             "featureType": "landscape",
+             "elementType": "geometry",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 20
+                         }]
+             }, {
+             "featureType": "road.highway",
+             "elementType": "geometry.fill",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 17
+                         }]
+             }, {
+             "featureType": "road.highway",
+             "elementType": "geometry.stroke",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 29
+                         }, {
+                         "weight": 0.2
+                         }]
+             }, {
+             "featureType": "road.arterial",
+             "elementType": "geometry",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 18
+                         }]
+             }, {
+             "featureType": "road.local",
+             "elementType": "geometry",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 16
+                         }]
+             }, {
+             "featureType": "poi",
+             "elementType": "geometry",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 21
+                         }]
+             }, {
+             "elementType": "labels.text.stroke",
+             "stylers": [{
+                         "visibility": "on"
+                         }, {
+                         "color": "#000000"
+                         }, {
+                         "lightness": 16
+                         }]
+             }, {
+             "elementType": "labels.text.fill",
+             "stylers": [{
+                         "saturation": 36
+                         }, {
+                         "color": "#000000"
+                         }, {
+                         "lightness": 40
+                         }]
+             }, {
+             "elementType": "labels.icon",
+             "stylers": [{
+                         "visibility": "off"
+                         }]
+             }, {
+             "featureType": "transit",
+             "elementType": "geometry",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 19
+                         }]
+             }, {
+             "featureType": "administrative",
+             "elementType": "geometry.fill",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 20
+                         }]
+             }, {
+             "featureType": "administrative",
+             "elementType": "geometry.stroke",
+             "stylers": [{
+                         "color": "#000000"
+                         }, {
+                         "lightness": 17
+                         }, {
+                         "weight": 1.2
+                         }]
+             }]
     };
-
+    
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
-
+    
     // Create the Google Map using out element and options defined above
     map = new google.maps.Map(mapElement, mapOptions);
-
+    
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
     var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
     var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
+                                             position: myLatLng,
+                                             map: map,
+                                             icon: image
+                                             });
 }
